d1f561da5bf8dd0d1e0f6772c1f86ce8c9b9f17f
moostafaa <mostafavakily@gmail.com>
2020-10-11 23:34:35 +0330

    Custom view engine

---
+++ Plugin.Widgets.QuickFilter/ViewEngine/CustomCatalogViewEngine.cs
@@ -0,0 +1,32 @@
+﻿using Nop.Web.Framework.Themes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Plugin.Widgets.QuickFilter.ViewEngine
+{
+    public class CustomCatalogViewEngine : IViewLocationExpander//I'm trying to change default search path of razor
+    {
+        public void PopulateValues(ViewLocationExpanderContext context)
+        {
+            if (context.AreaName?.Equals("Admin"))
+            {
+                IThemeContext theme = context.ActionContext.HttpContext.RequestServices.GetService(typeof(IThemeContext));
+                context.Values["ThemeName"] = theme.WorkingThemeName;
+            }
+        }
+
+        public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
+        {
+            if (context.Values.TryGetValue("", out string Value) && context.ControllerName.Equals("QuickFilter"))
+            {
+                //put here view and theme location
+                viewLocations = new string[2] {
+                    "",
+                    ""
+                }.Concat(viewLocations);
+            }
+        }
+    }
+}
